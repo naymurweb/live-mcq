d270e2ea48ad6b6370cc7d55bbbc6f449c0bac7a
naymurweb <naymurweb@gmail.com>
2022-11-21 23:29:15 +0600

    git problem

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+
+import { createBrowserRouter,RouterProvider } from 'react-router-dom';
 import './App.css';
+import Main from './Layout/Main';
 
 function App() {
+  const router=createBrowserRouter([
+    {path:'/',element:<Main></Main>}
+  ])
   return (
     <div className="App">
-      
+     <RouterProvider router={router}></RouterProvider>
+  
     </div>
   );
 }
